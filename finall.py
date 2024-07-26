56fe8ffb775788335cd0ed54d1d95a7c2008f272
Park Jae Hong <105151063+PHJ2000@users.noreply.github.com>
2024-07-26 10:49:34 +0900

    Update finall.py

--- finall.py
+++ finall.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import joblib
+# import joblib
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
